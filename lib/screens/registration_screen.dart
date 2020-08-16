c9699b4da9425469c9390f557ee80d3470eb6ea2
Uday Garg <u.garg14@gmail.com>
2020-08-16 13:37:34 +0530

    setting up firestore and added obscure text in password field,change package name, updated gradle files for firestore dependency

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -9,53 +9,64 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  String email;
+  String password;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: Container(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
+    return GestureDetector(
+      onTap: () {
+        print('ontap clicked');
+        FocusScope.of(context).requestFocus(new FocusNode());
+      },
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: 'logo',
+                child: Container(
+                  height: 200.0,
+                  child: Image.asset('images/logo.png'),
+                ),
               ),
-            ),
-            SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              onChanged: (value) {
-                //Do something with the user input.
-              },
-              decoration: kTextFieldInputDecoration.copyWith(
-                hintText: 'Enter your email',
+              SizedBox(
+                height: 48.0,
               ),
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              onChanged: (value) {
-                //Do something with the user input.
-              },
-              decoration: kTextFieldInputDecoration.copyWith(
-                  hintText: 'Enter your password'),
-            ),
-            SizedBox(
-              height: 24.0,
-            ),
-            LoginRegisterWidget(
-              onPressed: () {},
-              color: Colors.blueAccent,
-              text: 'Register',
-            ),
-          ],
+              TextField(
+                onChanged: (value) {
+                  email = value;
+                  print(email);
+                },
+                decoration: kTextFieldInputDecoration.copyWith(
+                  hintText: 'Enter your email',
+                ),
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                onChanged: (value) {
+                  password = value;
+                  print(password);
+                },
+                decoration: kTextFieldInputDecoration.copyWith(
+                    hintText: 'Enter your password'),
+                obscureText: true,
+              ),
+              SizedBox(
+                height: 24.0,
+              ),
+              LoginRegisterWidget(
+                onPressed: () {},
+                color: Colors.blueAccent,
+                text: 'Register',
+              ),
+            ],
+          ),
         ),
       ),
     );
